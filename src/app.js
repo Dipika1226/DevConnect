5851101ec536dd3636bc5434ad16c8e02a3af83a
Dipika1226 <dipika26cs044@satiengg.in>
2025-05-30 09:15:27 +0530

    .

--- src/app.js
+++ src/app.js
@@ -5,17 +5,18 @@ const cookieParser = require("cookie-parser");
 const jwt = require('jsonwebtoken');
 const cors = require("cors");
 app.set("trust proxy", 1);
-app.use(cors({
+const corsOptions = {
     origin: [
         'https://dev-connect-web-eight.vercel.app',
-        'http://localhost:5173'
+        'http://localhost:5173',
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
